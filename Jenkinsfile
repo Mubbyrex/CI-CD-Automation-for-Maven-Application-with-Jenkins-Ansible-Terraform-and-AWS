faa0645e2c6f0f2019109526507d65f662e62ac4
Mubarak <87546824+Mubbyrex@users.noreply.github.com>
2023-03-08 20:17:19 +0000

    pushing docker image via scripts

--- Jenkinsfile
+++ Jenkinsfile
@@ -16,8 +16,7 @@ pipeline {
         stage("build jar") {
             steps {
                 script {
-                    echo "building the application..."
-                    sh 'mvn package'
+                    gv.buildjar
                 }
             }
         }
--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildjar() {
 def buildImage() {
     echo "building docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t mubbyrex/jenkins-demo:jma-3.0 .'
+        sh 'docker build -t mubbyrex/jenkins-demo:jma-4.0 .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push mubbyrex/jenkins-demo:jma-3.0'
+        sh 'docker push mubbyrex/jenkins-demo:jma-4.0'
     }
 }
 

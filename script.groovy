a68baf252423f07573f7f856e0c605e665f0d1e2
Mubarak <87546824+Mubbyrex@users.noreply.github.com>
2023-03-08 16:49:19 +0000

    sighs

--- script.groovy
+++ script.groovy
@@ -1,17 +1,17 @@
 def buildjar() {
-        echo "building the application..."
-        sh 'mvn package'
+    echo "building the application..."
+    sh 'mvn package'
 }
 
 def buildImage() {
-        echo "building docker image..."
-        withCredentials([usernamePassword(credentialsId: 'dockerhub',passwordVariable:'PASSWORD',usernameVariable:'USERNAME')]){
-            sh 'docker build -t mubbyrex/jenkins-demo:jma-2.0 .'
-            sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-            sh 'docker push mubbyrex/jenkins-demo:jma-2.0'
-        }
+    echo "building docker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh 'docker build -t mubbyrex/jenkins-demo:jma-3.0 .'
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh 'docker push mubbyrex/jenkins-demo:jma-3.0'
+    }
 }
 
 def deployApp() {
-        echo 'deploying the app....'
+    echo 'deploying the app....'
 }

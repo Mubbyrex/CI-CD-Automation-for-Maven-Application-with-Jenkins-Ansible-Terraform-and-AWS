9721fb38136ef9e56e742d6951c4af3b0392d142
mubyrex <mubyre6xx@gmail.com>
2024-10-12 15:42:45 +0100

    add dynamic versioning

--- script.groovy
+++ script.groovy
@@ -21,6 +21,28 @@ def deployAppOnEC2() {
     }
 }
 
+def incrementVersion() {
+    echo 'increasing the app version...'
+    sh 'mvn build-helper:parse-version versions:set \
+        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+        versions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+}
+
+def commitVersion() {
+     withCredentials([usernamePassword(credentialsId: 'github-pat', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh 'git config --global user.email "jenkins@example.com"'
+        sh 'git config --global user.name "jenkins"'
+
+        sh "git remote set-url origin https://${USERNAME}:${PASSWORD}@github.com/Mubbyrex/Jenkins-Project.git"
+        sh 'git add .'
+        sh 'git commit -m "ci: version bump"'
+        sh 'git push origin HEAD:main'
+     }
+}
+
 def deployAppOnK8s(){
     echo 'deploying docker image...'
     sh 'kubectl create deployment nginx-deployment --image=nginx'
--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -9,16 +9,8 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @PostConstruct
-    public void init()
-    {
-        Logger log = LoggerFactory.getLogger(Application.class);
-        log.info("Java app started");
     }
 
     public String getStatus() {
